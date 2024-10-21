f7059f795a93525962f4d4c4926790d142a0bc1d
Luis Lopes <lopez.assis_@outlook.com>
2024-10-21 17:44:44 -0300

    capítulo 05 - exercício 04

---
+++ Python/Módulo Basico/cap05-ex04.py
@@ -0,0 +1,12 @@
+D = int(input('Digite D: '))
+
+if D <= 0:
+    print(f'O valor {D} é inválido.')
+else:
+    i = 1
+    while i < 100:
+        if i % D == 0:
+            print(i, end='  ')
+        i += 1
+
+print('Fim do programa.')
