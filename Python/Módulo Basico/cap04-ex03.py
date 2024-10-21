b0146cfd514c5d6a720fadcd162c25272de499d8
Luis Lopes <lopez.assis_@outlook.com>
2024-10-21 17:15:17 -0300

    capÃítulo 04 -e xercício 03

---
+++ Python/Módulo Basico/cap04-ex03.py
@@ -0,0 +1,11 @@
+A = int(input('Digite A: '))
+B = int(input('Digite B: '))
+
+if A == B:
+    print(f'Os dois números são iguais e valem {A}.')
+elif A < B:
+    print(f'O menor número é {A}.')
+else:
+    print(f'O menor número é {B}.')
+print('Fim do programa.')
+
