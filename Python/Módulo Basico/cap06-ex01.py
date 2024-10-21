b63fe8d6dd8aa94eb86cd873ecf05bf5836681d2
Luis Lopes <lopez.assis_@outlook.com>
2024-10-21 18:09:54 -0300

    capítulo 06 - exercício 01

---
+++ Python/Módulo Basico/cap06-ex01.py
@@ -0,0 +1,10 @@
+A = int(input('Digite A: '))
+B = int(input('Digite B: '))
+
+try:
+    R = A / B
+    print(R)
+except:
+    print('Não é possível calcular a divisão.')
+
+print('Fim do programa.')
