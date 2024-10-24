62a7ada2bb4cb107f421427be57446a795cf0ded
Luís Gustavo de Assis Lopes <luis.lopes@ihs-matriz.com.br>
2024-10-24 10:23:24 -0300

    capítulo 8 - exemplo match case

---
+++ Módulo Intermediário/Exemplo8.1.py
@@ -0,0 +1,15 @@
+n = -1
+while n != 0:
+    n = int(input('Digite um inteiro: '))
+    match n:
+        case 1: 
+            print('Você digitou um.')
+        case 2: 
+            print('Você digitou dois.')
+        case 3:
+            print('Você digitou três.')
+        case _: 
+            print('Você digitou outro número')
+
+print('Fim do programa')
+    
