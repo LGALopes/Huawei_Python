409619edb66feabdb810648bd1a3194aaa5e675d
Luis Lopes <lopez.assis_@outlook.com>
2024-11-17 19:37:11 -0300

    capitulo 11 - exercicio 06

---
+++ Módulo Intermediário/cap11-ex06.py
@@ -0,0 +1,22 @@
+Estoque = {}
+for linha in open('entrada_er_11.6.txt', 'r'):
+    lst = linha.rstrip().split(';')
+    print(lst)
+    cod = int(lst[0])
+    qtde = int(lst[1])
+    pcunit = float(lst[2])
+    Estoque[cod] = (qtde, pcunit)
+
+print('Valores carregados no dicionário.')
+print(Estoque)
+
+print('Exibição dos dados na forma de tabela.')
+TotGeral = 0
+for cod, dados in Estoque.items():
+    tot = dados[0] * dados[1]
+    TotGeral += tot
+    print(f'    {cod}: {dados[0]:5d} x {dados[1]:6.2f} = {tot:8.2f}')
+
+print(' '  * 24, f'{TotGeral:8.2f}') 
+
+print('\nFim do programa')
